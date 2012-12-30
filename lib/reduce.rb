74068c7a151ff06f887088819b1b346b922ac2c0
Pacbard <bardellie@gmail.com>
2012-12-29 19:51:37 -0500

    added js compression with closure compiler

--- lib/reduce.rb
+++ lib/reduce.rb
@@ -8,21 +8,24 @@ module Reduce
     def reduce(file)
       extension = File.extname(file).downcase.sub('.','')
       case extension
-      when 'html', 'xml'
-        compressor = File.join(File.dirname(__FILE__),'..','vendor','htmlcompressor*.jar')
-        `java -jar #{compressor} --type #{extension} --compress-js --compress-css #{file}`
-      when 'js','css'
-        compressor = File.join(File.dirname(__FILE__),'..','vendor','yuicompressor*.jar')
-        `java -jar #{compressor} --type #{extension} #{file}`
-      when 'jpg', 'jpeg', 'png', 'gif'
-        reduce_image file
-      else
-        raise "reduce does not know how to handle a .#{extension} file (#{file})"
+        when 'html', 'xml'
+          compressor = File.join(File.dirname(__FILE__),'..','vendor','htmlcompressor*.jar')
+          `java -jar #{compressor} --type #{extension} --compress-js --compress-css --js-compressor closure #{file}`
+        when 'js'
+          compressor = File.join(File.dirname(__FILE__),'..','vendor','compiler.jar')
+          `java -jar #{compressor} --js #{file}`
+        when 'css'
+          compressor = File.join(File.dirname(__FILE__),'..','vendor','yuicompressor*.jar')
+          `java -jar #{compressor} --type #{extension} #{file}`
+        when 'jpg', 'jpeg', 'png', 'gif'
+          reduce_image file
+        else
+          raise "reduce does not know how to handle a .#{extension} file (#{file})"
+        end
       end
-    end
 
     private
-
+    
     def reduce_image(input)
       output = input+'.temp'
       FileUtils.cp(input, output)
